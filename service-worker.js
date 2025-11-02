5805330f82a03371c06d00fc28be3664d0b56031
kimkimkim0000 <kimsl160614@gmail.com>
2025-11-02 13:45:40 +0900

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install',e=>{
- e.waitUntil(caches.open('kwbob-cache').then(c=>c.addAll(['index.html','style.css','script.js','manifest.json'])))
-});
-self.addEventListener('fetch',e=>{
- e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))
-});
